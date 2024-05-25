2038159b3588f759562b1d718a7179b08c7d681a
Паутов Александр <sh2d000w@gmail.com>
2024-05-25 20:18:23 +0300

    fix: Change result to function in default create_date.

--- app/models/base.py
+++ app/models/base.py
@@ -10,5 +10,5 @@ class BaseProjectDonation(Base):
     full_amount = Column(Integer, nullable=False)
     invested_amount = Column(Integer, default=0)
     fully_invested = Column(Boolean, default=False)
-    create_date = Column(DateTime, default=datetime.now())
+    create_date = Column(DateTime, default=datetime.now)
     close_date = Column(DateTime)
