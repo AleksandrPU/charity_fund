d067a07ee47403a1be4412a755dedb88dd287ea0
Паутов Александр <sh2d000w@gmail.com>
2024-05-25 18:35:02 +0300

    feat: Add delete and patch endpoints for CharityProject.

--- app/api/endpoints/charityproject.py
+++ app/api/endpoints/charityproject.py
@@ -2,11 +2,21 @@ from fastapi import APIRouter, Depends
 from fastapi.encoders import jsonable_encoder
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.api.validators import check_name_duplicate
+from app.api.validators import (
+    check_name_duplicate,
+    check_project_exists,
+    check_project_open,
+    check_project_full_amount,
+    check_project_empty,
+)
 from app.core.db import get_async_session
 from app.core.user import current_superuser
 from app.crud.charityproject import charity_project_crud
-from app.schemas.charityproject import CharityProjectCreate, CharityProjectDB
+from app.schemas.charityproject import (
+    CharityProjectCreate,
+    CharityProjectDB,
+    CharityProjectUpdate,
+)
 
 router = APIRouter()
 
@@ -36,3 +46,46 @@ async def create_project(
     await check_name_duplicate(charity_project.name, session)
     new_project = await charity_project_crud.create(charity_project, session)
     return new_project
+
+
+@router.delete(
+    '/{project_id}',
+    response_model=CharityProjectDB,
+    response_model_exclude_none=True,
+    dependencies=[Depends(current_superuser)]
+)
+async def delete_project(
+        project_id: int,
+        session: AsyncSession = Depends(get_async_session)
+):
+    project = await check_project_exists(project_id, session)
+    await check_project_open(project)
+    await check_project_empty(project)
+
+    project = await charity_project_crud.remove(project, session)
+    return jsonable_encoder(project)
+
+
+@router.patch(
+    '/{project_id}',
+    response_model=CharityProjectDB,
+    response_model_exclude_none=True,
+    dependencies=[Depends(current_superuser)]
+)
+async def update_project(
+        project_id: int,
+        project_in: CharityProjectUpdate,
+        session: AsyncSession = Depends(get_async_session)
+):
+    project = await check_project_exists(project_id, session)
+    await check_project_open(project)
+
+    if project_in.name is not None:
+        await check_name_duplicate(project_in.name, session)
+
+    if project_in.full_amount is not None:
+        await check_project_full_amount(project, project_in.full_amount)
+
+    project = await charity_project_crud.update(project, project_in, session)
+
+    return jsonable_encoder(project)
