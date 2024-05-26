3e49611c75bcdabfb472413b07b032e654f7e823
Паутов Александр <sh2d000w@gmail.com>
2024-05-26 20:43:16 +0300

    fix: PEP8 in __init__.py.

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,4 +2,8 @@ from .charity_project import CharityProject
 from .donation import Donation
 from .user import User
 
-__all__ = [CharityProject, Donation, User]
+__all__ = [
+    'CharityProject',
+    'Donation',
+    'User',
+]
