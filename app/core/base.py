96536f18b26e5f82e7afe0f7c3f051733e4f3206
Паутов Александр <sh2d000w@gmail.com>
2024-05-26 20:45:22 +0300

    fix: PEP8 in core/base.py.

--- app/core/base.py
+++ app/core/base.py
@@ -1,3 +1,10 @@
 """Для Alembic."""
-from app.core.db import Base  # noqa
-from app.models import CharityProject, Donation, User  # noqa
+from app.core.db import Base
+from app.models import CharityProject, Donation, User
+
+__all__ = [
+    'Base',
+    'CharityProject',
+    'Donation',
+    'User',
+]
