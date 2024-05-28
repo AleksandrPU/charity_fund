ec9d90849402f2fffbd652f24ca65dd08df866a8
Паутов Александр <sh2d000w@gmail.com>
2024-05-28 13:17:00 +0300

    feat: Chande SQLite to PostgresSQL.

--- app/core/config.py
+++ app/core/config.py
@@ -6,9 +6,14 @@ from pydantic import BaseSettings, EmailStr
 class Settings(BaseSettings):
     app_title: str = 'QRKot'
     app_description: str = 'Фонд собирает пожертвования на различные проекты.'
-    database_url: str = 'sqlite+aiosqlite:///./fastapi.db'
+    # database_url: str = 'sqlite+aiosqlite:///./fastapi.db'
+    postgres_user: str = 'fastapi'
+    postgres_password: str = 'mysecretpassword'
+    postgres_db: str = 'fastapi'
+    database_url: str = 'postgresql+asyncpg://fastapi:mysecretpassword@db:5432/fastapi'
     secret: str = 'secret'
-    token_lifetime: int = 3600
+    # token_lifetime: int = 3600
+    token_lifetime: int = 360000
     first_superuser_email: Optional[EmailStr] = None
     first_superuser_password: Optional[str] = None
 
