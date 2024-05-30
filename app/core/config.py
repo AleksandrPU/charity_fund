fdceb388d3c2c04e9966f4365f197d6e7baabc00
Паутов Александр <sh2d000w@gmail.com>
2024-05-30 16:01:33 +0300

    feat: Change database setup in config.py.

--- app/core/config.py
+++ app/core/config.py
@@ -7,6 +7,13 @@ class Settings(BaseSettings):
     app_title: str = 'QRKot'
     app_description: str = 'Фонд собирает пожертвования на различные проекты.'
     database_url: str = 'sqlite+aiosqlite:///./fastapi.db'
+<<<<<<< Updated upstream
+=======
+    # postgres_user: str = 'fastapi'
+    # postgres_password: str = 'mysecretpassword'
+    # postgres_db: str = 'fastapi'
+    # database_url: str = 'postgresql+asyncpg://fastapi:mysecretpassword@db:5432/fastapi'
+>>>>>>> Stashed changes
     secret: str = 'secret'
     token_lifetime: int = 3600
     first_superuser_email: Optional[EmailStr] = None
