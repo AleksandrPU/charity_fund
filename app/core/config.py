b3fabe1412e2aa7f1030f225cf28249bfc7be3f1
Паутов Александр <sh2d000w@gmail.com>
2024-06-01 13:19:48 +0300

    fix: Change database_url in config.py.

--- app/core/config.py
+++ app/core/config.py
@@ -1,28 +1,12 @@
 from typing import Optional
 
 from pydantic import BaseSettings, EmailStr
-from sqlalchemy.engine import URL
 
 
 class Settings(BaseSettings):
     app_title: str = 'QRKot'
     app_description: str = 'Фонд собирает пожертвования на различные проекты.'
-    # database_url: str = 'sqlite+aiosqlite:///./fastapi.db'
-    # database_url: str = 'postgresql+asyncpg://fastapi:mysecretpassword@db:5432/fastapi'
-    # dialect+driver://username:password@host:port/database
-    database_dialect_driver: str = 'sqlite+aiosqlite'
-    database_username: str = None
-    database_password: str = None
-    database_host: str = None
-    database_port: int = None
-    database_db: str = './fastapi.db'
-    database_url: URL = URL.create(
-        database_dialect_driver,
-        username=database_username,
-        password=database_password,
-        host=database_host,
-        database=database_db,
-    )
+    database_url: str = 'sqlite+aiosqlite:///./fastapi.db'
     secret: str = 'secret'
     token_lifetime: int = 3600
     first_superuser_email: Optional[EmailStr] = None
