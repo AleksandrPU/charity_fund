cc687770314902251aae8d2686840935248166a1
Паутов Александр <sh2d000w@gmail.com>
2024-06-01 13:04:15 +0300

    fix: Fix CRUDs.

--- app/crud/base.py
+++ app/crud/base.py
@@ -1,9 +1,9 @@
-from typing import Optional
+from typing import Optional, Union
 
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.models import User
+from app.models import CharityProject, Donation, User
 
 
 class CRUDBase:
@@ -11,22 +11,11 @@ class CRUDBase:
     def __init__(self, model):
         self.model = model
 
-    async def create_object(
-            self,
-            obj_in,
-            user: Optional[User] = None,
-    ):
-        obj_in_data = obj_in.dict()
-        if user is not None:
-            obj_in_data['user_id'] = user.id
-
-        return self.model(**obj_in_data)
-
     async def get_multi(
             self,
             session: AsyncSession,
             not_full_invested: bool = False
-    ):
+    ) -> list[Union[CharityProject, Donation]]:
         query = select(self.model)
         if not_full_invested:
             query = query.where(~self.model.fully_invested)
@@ -38,16 +27,14 @@ class CRUDBase:
             obj_in,
             session: AsyncSession,
             user: Optional[User] = None,
-    ):
+    ) -> Union[CharityProject, Donation]:
         obj_in_data = obj_in.dict()
         if user is not None:
             obj_in_data['user_id'] = user.id
 
         db_obj = self.model(**obj_in_data)
 
-        # from app.services.charity_project import CharityProjectService
-        # await CharityProjectService(session).add_to_queue(db_obj)
-
+        session.add(db_obj)
         await session.commit()
         await session.refresh(db_obj)
 
